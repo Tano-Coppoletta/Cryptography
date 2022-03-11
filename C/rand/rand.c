9c7570aee1a02afebf837070577eabf5f2d4e8d7
Gaetano Coppoletta s301131 <s301131@studenti.polito.it>
2022-03-11 18:12:25 +0100

    a

--- C/rand/rand.c
+++ C/rand/rand.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
 #include <openssl/rand.h>
+#include <openssl/err.h>
 
 #define MAX 128
 
+void handle_errors(){
+    ERR_print_errors_fp(stderr);
+    abort();
+}
+
 int main(){
     unsigned char random_string[MAX];
-    RAND_bytes(random_string,MAX);
+
+    if(RAND_load_file('/dev/random',64) != 64) 
+        handle_errors();
+       
+    if(RAND_bytes(random_string,MAX) != 1)
+        handle_errors();
 
     for(int i=0;i<MAX;i++)
         printf("%02x-",random_string[i]);
